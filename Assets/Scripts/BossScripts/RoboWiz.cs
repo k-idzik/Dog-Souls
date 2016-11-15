8fd727f119e2d0110593512442965cd36d77fa56
Kevin Idzik <kdi4293@rit.edu>
2016-11-14 23:59:53 -0500

    Boss Destroys Shields

--- Assets/Scripts/BossScripts/RoboWiz.cs
+++ Assets/Scripts/BossScripts/RoboWiz.cs
@@ -1,18 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; //Lists
 
 public class RoboWiz : MonoBehaviour
 {
-    private enum Phases { PHASE1, PHASE2, PHASE3, PHASE4 }; //Finite state machine
+    private bool[] phases = new bool[4] { false, false, false, true }; //Array of booleans to hold the current state
     [SerializeField] private int health; //The player's health
-    [SerializeField] private GameObject[] shields; //The shields
+    [SerializeField] private List<GameObject> shields; //The shields
     [SerializeField] private GameObject magicMissiles; //The boss's magic missiles
     [SerializeField] private float timeBetweenAttacks; //The time between the boss's attacks
-    [SerializeField] private float timeAttacks; //The timeing for the boss's attacks
+    [SerializeField] private float missileAttacks; //The timing for the boss's attacks
 
     void Update() //Update is called once per frame
     {
-        if (health % 25 == 0.0f) //If the boss's health is down by an even quarter
+        if (!phases[0] && !phases[(health - 25) / 25]) //If the boss's health is down by an even quarter
         {
             PhaseChange(); //Change the phase
         }
@@ -45,6 +46,14 @@ public class RoboWiz : MonoBehaviour
 
     private void PhaseChange() //When the boss should change phases
     {
+        phases[(health - 25) / 25] = true; //Activate the next phase
+        phases[((health - 25) / 25) + 1] = false; //Deactivate the previous phase
 
+        if (shields.Count > 0) //If there are still shields left
+        {
+            int shieldNumber = Random.Range(0, shields.Count); //Get the shield to destroy
+            Destroy(shields[shieldNumber]); //Destory the shield
+            shields.RemoveAt(shieldNumber); //Remove the shield from the array
+        }
     }
 }
