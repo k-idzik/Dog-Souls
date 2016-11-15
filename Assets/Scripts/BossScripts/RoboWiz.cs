0dbd4460c361b5efb42822b7e65979bf4b520c8d
Kevin Idzik <kdi4293@rit.edu>
2016-11-14 19:37:58 -0500

    Boss Method Update

--- Assets/Scripts/BossScripts/RoboWiz.cs
+++ Assets/Scripts/BossScripts/RoboWiz.cs
@@ -1,15 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RoboWiz : MonoBehaviour {
+public class RoboWiz : MonoBehaviour
+{
+    private enum Phases { PHASE1, PHASE2, PHASE3, PHASE4 }; //Finite state machine
+    private int health; //The player's health
+    [SerializeField] private GameObject[] shields; //The shields
+    [SerializeField] private GameObject magicMissiles; //The boss's magic missiles
+    [SerializeField] private float timeBetweenAttacks; //The time between the boss's attacks
+    [SerializeField] private float timeAttacks; //The timeing for the boss's attacks
 
-	// Use this for initialization
-	void Start () {
-	
+    void Start() //Use this for initialization
+    {
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+    IEnumerator Cooldown(float time) //Co-routine timer
+    {
+        yield return new WaitForSeconds(time); //Timer
+    }
+
+    void Update() //Update is called once per frame
+    {
+        if (health % 25 == 0.0f) //If the boss's health is down by an even quarter
+        {
+            PhaseChange(); //Change the phase
+        }
+
+        if(health == 0) //If the boss is dead
+        {
+            Destroy(gameObject); //He dead
+        }
 	}
+
+    private void FirstAttack() //The boss's first attack, sprinkler
+    {
+
+    }
+
+    private void SecondAttack() //The boss's second attack, minion spawning
+    {
+
+    }
+
+    private void ThirdAttack() //The boss's third attack
+    {
+
+    }
+
+    private void PhaseChange() //When the boss should change phases
+    {
+
+    }
 }
