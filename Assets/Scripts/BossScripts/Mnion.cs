ebd9dbd17710bcc61f7d1e230e0af8e4245d9885
Charles Williams <crw4147@rit.edu>
2016-11-21 23:05:36 -0500

    Phase 2

---
+++ Assets/Scripts/BossScripts/Mnion.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mnion : MonoBehaviour {
+
+    //-----ATTRIBUTES-----
+    [SerializeField]
+    protected float knockbackScale;
+    public float moveSpeed;
+    public int health;
+    public int damage;
+    protected Rigidbody2D rb;
+    public float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        Move();
+	}
+
+    /// <summary>
+    /// responsible for moving the enemy
+    /// </summary>
+    public void Move()
+    {
+        Vector3 unitOffset = Seek();
+        transform.up = unitOffset;
+        Vector3 acceleration = unitOffset * moveSpeed;
+        rb.AddForce(acceleration);
+        float velX = Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed);
+        float velY = Mathf.Clamp(rb.velocity.y, -maxSpeed, maxSpeed);
+
+        rb.velocity = new Vector3(velX, velY, 0);
+    }
+
+    /// <summary>
+    /// Method resposible for seeking the player
+    /// </summary>
+    /// <returns>offset vector</returns>
+    public Vector3 Seek()
+    {
+        Vector3 offset = GameObject.FindGameObjectWithTag("Player").transform.position - this.transform.position;
+        Vector3 unitOffset = offset.normalized;
+        return unitOffset;
+    }
+
+    /// <summary>
+    /// to check if the enemy is colliding with a player
+    /// </summary>
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //if (coll.transform.tag == "Player")
+        //{
+        // knockback along inverted up vector
+        rb.AddForce(-1 * transform.up * knockbackScale);
+
+        //}
+    }
+
+    private void OnTriggerEnter2D(Collider2D coll) //When something collides with the player
+    {
+        if(coll.gameObject.tag == "weapon") //If the enemy is colliding with the player's weapon.
+        {
+            health--;
+        }
+    }
+}
--- Assets/Scripts/BossScripts/RoboWiz.cs
+++ Assets/Scripts/BossScripts/RoboWiz.cs
@@ -4,15 +4,19 @@ using System.Collections.Generic; //Lists
 
 public class RoboWiz : Boss
 {
-    private bool[] phases = new bool[4] { false, false, false, true }; //Array of booleans to hold the current state
+    private bool[] phases = new bool[4] { false, false, true, false }; //Array of booleans to hold the current state
     [SerializeField] private List<GameObject> shields; //The shields
     [SerializeField] private GameObject magicMissile; //The boss's magic missiles
+    [SerializeField]
+    private GameObject minion;
     [SerializeField] private float timeBetweenAttacks; //The time between the boss's attacks
     [SerializeField] private float missileAttacks; //The timing for the boss's attacks
     [SerializeField] private float numMissiles; // number of missiles
     [SerializeField]
     private float missileCooldown; // time between missile barrages
-    private float missileTimer = 0; // to count down between barrages of missiles
+    private float missileTimer; // to count down between barrages of missiles
+    private float minionTimer;
+    [SerializeField] private float minionCooldown;
     private float missileAngle; // current angle of the missile, should always start at 0
     private float angleBetweenMissiles; // angle between each missile, should be 360 / numMissiles
     private float numSpawned; // counter for the number of missiles spawned in each attack
@@ -27,6 +31,10 @@ public class RoboWiz : Boss
 
         // calculate angle between missiles spawning
         angleBetweenMissiles = 360 / numMissiles;
+
+        missileTimer = missileCooldown;
+
+        minionTimer = minionCooldown;
     } 
     protected override void Update() //Update is called once per frame
     {
@@ -46,6 +54,34 @@ public class RoboWiz : Boss
             missileTimer -= Time.deltaTime;
 
             //Debug.Log(missileTimer);
+        }
+
+        // SECOND PHASE
+        if(phases[2] == true)
+        {
+            if (missileTimer <= 0)
+            {
+                // call first attack if timer is up
+                FirstAttack();
+
+                // reset timer
+                missileTimer = missileCooldown;
+            }
+
+            // decrement the timer
+            missileTimer -= Time.deltaTime;
+
+            if (minionTimer <= 0)
+            {
+                // call second attack if timer is up
+                SecondAttack();
+
+                // reset timer
+                minionTimer = minionCooldown;
+            }
+
+            // decrement timer
+            minionTimer -= Time.deltaTime;
         }
 
         if (health <= 75 && health > 0 && !phases[health / 25]) //If the boss's health is down by an even quarter
@@ -91,6 +127,11 @@ public class RoboWiz : Boss
 
     private void SecondAttack() //The boss's second attack, minion spawning
     {
+        float randx = Random.Range(GameObject.FindGameObjectWithTag("Player").transform.position.x - 5, GameObject.FindGameObjectWithTag("Player").transform.position.x + 5);
+        float randy = Random.Range(GameObject.FindGameObjectWithTag("Player").transform.position.y - 5, GameObject.FindGameObjectWithTag("Player").transform.position.y + 5);
+
+        // instantiate new enemy at random location
+        Instantiate(minion, new Vector3(randx, randy, 0), Quaternion.identity);
 
     }
 
