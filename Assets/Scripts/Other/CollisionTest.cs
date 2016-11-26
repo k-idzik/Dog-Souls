e29f2ba1c6d9e9a6386271b14ebced8970cccac2
Kevin Idzik <kdi4293@rit.edu>
2016-11-26 16:04:10 -0500

    BossRoom2

---
+++ Assets/Scripts/Other/CollisionTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTest : MonoBehaviour
+{	
+	void Start() //Use this for initialization
+    {
+	
+	}
+	
+	void Update() //Update is called once per frame
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll) //If something triggers this object
+    {
+        Debug.Log("Trigger: " + coll.gameObject.name); //Log the trigger
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) //If something collides with this object
+    {
+        Debug.Log("Collider: " + coll.gameObject.name); //Log the collision
+    }
+}
