129e5b29e61ed8c8764ea48424a7c8d3a8d785a0
Mark Obeldobel <mjo5501@rit.edu>
2016-11-28 16:40:27 -0500

    Added some color changes. Viola!

--- Assets/Scripts/BossScripts/BirdInTheNight/EnergyTowers.cs
+++ Assets/Scripts/BossScripts/BirdInTheNight/EnergyTowers.cs
@@ -6,16 +6,22 @@ public class EnergyTowers : MonoBehaviour {
     public SpriteRenderer sprite;
     public Color baseColor;
     public Color hitColor;
+    public Color settleColor;
     public double currentTime;
     public double savedTime;
+    float blueInc;
+    float greenInc;
 
 	// Use this for initialization
 	void Start () {
         sprite = this.GetComponent<SpriteRenderer>();
         baseColor = new Color(1f, 1f, 1f, 1f);
         hitColor = new Color(0.7f, 0f, 0f, 1f);
+        settleColor = new Color(0.7f, 0.3f, 0.3f);
         savedTime = 0;
         currentTime = 0;
+        blueInc = 0.01f;
+        greenInc = 0.01f;
     }
 	
 	// Update is called once per frame
@@ -24,6 +30,10 @@ public class EnergyTowers : MonoBehaviour {
         if (currentTime >= savedTime + 10)
         {
             sprite.color = baseColor;
+        }
+        if (sprite.color != baseColor && sprite.color != settleColor)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g + greenInc, sprite.color.b + blueInc);
         }
 	}
 
@@ -34,11 +44,15 @@ public class EnergyTowers : MonoBehaviour {
             sprite.color = hitColor;
             savedTime = currentTime;
         }
+        if (coll.gameObject.tag == "weapon")
+        {
+            savedTime = currentTime;
+        }
     }
 
     public bool isHit()
     {
-        if (sprite.color == hitColor)
+        if (sprite.color != baseColor)
         {
             return true;
         }
