e696477df25e0ccdc2190a4c75094e28b5ad7271
Kevin Idzik <kdi4293@rit.edu>
2016-12-13 07:19:47 -0500

    Win Condition

--- Assets/Scripts/Environment/LoadScene.cs
+++ Assets/Scripts/Environment/LoadScene.cs
@@ -6,6 +6,14 @@ public class LoadScene : MonoBehaviour
 {
     private static string[] canLoadScene = new string[3] { "BossRoom0", "BossRoom1", "BossRoom2" }; //If this collider can load the scene
 
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "MainRoom" && canLoadScene[0] == "" && canLoadScene[1] == "" && canLoadScene[2] == "") //If the game has been won
+        {
+            SceneManager.LoadScene("Title"); //Load the scene
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D coll)
     {
         for (int i = 0; i < canLoadScene.Length; i++) //Loop through the list of scenes
